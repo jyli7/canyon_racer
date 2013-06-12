cf4231aa6bd6840e66c19e05483765e38decf02a
jimmyli <jyl702@gmail.com>
2013-06-12 15:33:07 -0400

    im a cop, you idiot

--- public/onload.js
+++ public/onload.js
@@ -1,5 +1,7 @@
 window.onload = function () {
 	// Run game loop when user hits enter
+	var theme = document.getElementById('theme');
+	theme.play();
 	var gameBegun = false;
 	addEventListener("keydown", function (e) {
 		if (e.keyCode == 13 && !gameBegun) {
